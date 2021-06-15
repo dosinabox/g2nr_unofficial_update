fc0f34af3385e101dfd16334dc4ab5f6763587ad
dosinabox <39790055+dosinabox@users.noreply.github.com>
2021-06-16 00:50:47 +0300

    16.06.2021

--- PrjGOTHIC/Story/B_Story/C_RengaruIsReadyToTalk.d
+++ PrjGOTHIC/Story/B_Story/C_RengaruIsReadyToTalk.d
@@ -8,17 +8,14 @@ func int C_RengaruIsReadyToTalk()
 	if(!Npc_KnowsInfo(other,DIA_Rengaru_HALLODIEB))
 	{
 		return TRUE;
-	}
-	else
+	};
+	if(Npc_GetDistToWP(self,"NW_CITY_HABOUR_KASERN_05_01") <= 700)
 	{
-		if(Npc_GetDistToWP(self,"NW_CITY_HABOUR_KASERN_05_01") <= 700)
-		{
-			return TRUE;
-		};
-		if(Npc_GetDistToWP(self,"NW_CITY_HABOUR_POOR_AREA_PATH_01_03") <= 700)
-		{
-			return TRUE;
-		};
+		return TRUE;
+	};
+	if(Npc_GetDistToWP(self,"NW_CITY_HABOUR_POOR_AREA_PATH_01_03") <= 700)
+	{
+		return TRUE;
 	};
 	return FALSE;
 };
