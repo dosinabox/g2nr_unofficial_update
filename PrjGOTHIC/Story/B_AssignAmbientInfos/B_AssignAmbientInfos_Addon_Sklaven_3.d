78855a3b18aa4cf434df7cff721a04599328e79b
dosinabox <39790055+dosinabox@users.noreply.github.com>
2026-02-07 19:15:50 +0100

    v32 - 07.02.2026

--- PrjGOTHIC/Story/B_AssignAmbientInfos/B_AssignAmbientInfos_Addon_Sklaven_3.d
+++ PrjGOTHIC/Story/B_AssignAmbientInfos/B_AssignAmbientInfos_Addon_Sklaven_3.d
@@ -1,36 +1,25 @@
 
-instance DIA_Skalven_3_EXIT(C_Info)
+instance DIA_Sklaven_3_EXIT(C_Info)
 {
 	nr = 999;
-	condition = DIA_Skalven_3_EXIT_Condition;
-	information = DIA_Skalven_3_EXIT_Info;
+	condition = DIA_Common_EXIT_Condition;
+	information = DIA_Common_EXIT_Info;
 	permanent = TRUE;
 	description = Dialog_Ende;
 };
 
 
-func int DIA_Skalven_3_EXIT_Condition()
-{
-	return TRUE;
-};
-
-func void DIA_Skalven_3_EXIT_Info()
-{
-	AI_StopProcessInfos(self);
-};
-
-
-instance DIA_Skalven_3_STANDARD(C_Info)
+instance DIA_Sklaven_3_STANDARD(C_Info)
 {
 	nr = 2;
-	condition = DIA_Skalven_3_STANDARD_Condition;
-	information = DIA_Skalven_3_STANDARD_Info;
+	condition = DIA_Sklaven_3_STANDARD_Condition;
+	information = DIA_Sklaven_3_STANDARD_Info;
 	permanent = TRUE;
 	important = TRUE;
 };
 
 
-func int DIA_Skalven_3_STANDARD_Condition()
+func int DIA_Sklaven_3_STANDARD_Condition()
 {
 	if(Npc_IsInState(self,ZS_Talk))
 	{
@@ -38,15 +27,15 @@ func int DIA_Skalven_3_STANDARD_Condition()
 	};
 };
 
-func void DIA_Skalven_3_STANDARD_Info()
+func void DIA_Sklaven_3_STANDARD_Info()
 {
-	var int randy;
-	randy = Hlp_Random(3);
-	if(randy == 0)
+	var int random;
+	random = Hlp_Random(3);
+	if(random == 0)
 	{
 		AI_Output(self,other,"DIA_Addon_Sklaven_3_STANDARD_03_00");	//(испуганно) Что ты хочешь от меня? Уйди прочь!
 	}
-	else if(randy == 1)
+	else if(random == 1)
 	{
 		AI_Output(self,other,"DIA_Addon_Sklaven_3_STANDARD_03_01");	//(нервно) Я ничего не делал! Оставь меня.
 	}
@@ -59,7 +48,7 @@ func void DIA_Skalven_3_STANDARD_Info()
 
 func void B_AssignAmbientInfos_Addon_Sklaven_3(var C_Npc slf)
 {
-	DIA_Skalven_3_EXIT.npc = Hlp_GetInstanceID(slf);
-	DIA_Skalven_3_STANDARD.npc = Hlp_GetInstanceID(slf);
+	DIA_Sklaven_3_EXIT.npc = Hlp_GetInstanceID(slf);
+	DIA_Sklaven_3_STANDARD.npc = Hlp_GetInstanceID(slf);
 };
 
